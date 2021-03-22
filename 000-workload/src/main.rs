5f9bae65a52f687cec6867cb4627615c07405047
David McKay <david@rawkode.com>
2021-03-22 17:13:04 +0000

    fix: listen on all interfaces

--- 000-workload/src/main.rs
+++ 000-workload/src/main.rs
@@ -8,7 +8,7 @@ async fn main() -> std::io::Result<()> {
             .service(fs::Files::new("/assets", "./assets"))
             .route("/", web::get().to(greet))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(("0.0.0.0", 8080))?
     .run()
     .await
 }
