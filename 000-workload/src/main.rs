403317b3640dc0087b707d01f3761958c856af23
David McKay <david@rawkode.com>
2021-03-22 16:51:27 +0000

    Workload

---
+++ 000-workload/src/main.rs
@@ -0,0 +1,31 @@
+use actix_files as fs;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(fs::Files::new("/assets", "./assets"))
+            .route("/", web::get().to(greet))
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
+}
+
+async fn greet(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok().body(
+        r#"
+        <html>
+        <head>
+            <title>v1 | Klustered</title>
+        </head>
+        <body>
+            <video width="1280" height="720" controls>
+            <source src="/assets/video.mp4" type="video/mp4">
+            </video>
+        </body>
+        </html>
+"#,
+    )
+}
