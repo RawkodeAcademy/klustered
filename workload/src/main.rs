63a01d9964465b8ac0837da2a3ff90fdf9710bc2
David McKay <david@rawkode.com>
2021-07-16 11:31:43 +0100

    chore: add back workload

---
+++ workload/src/main.rs
@@ -0,0 +1,103 @@
+use actix_files as fs;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
+use postgres::{Client, NoTls};
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(fs::Files::new("/assets", "./assets"))
+            .route("/", web::get().to(greet))
+            .route("/health", web::get().to(health))
+    })
+    .bind(("0.0.0.0", 8080))?
+    .run()
+    .await
+}
+
+// This is not a health check
+async fn health(_req: HttpRequest) -> impl Responder {
+    HttpResponse::Ok()
+}
+
+async fn greet(_req: HttpRequest) -> impl Responder {
+    let sql =
+        r#"SELECT * FROM quotes OFFSET floor(random() * (SELECT COUNT(*) FROM quotes)) LIMIT 1;"#;
+
+    let mut client = match Client::connect(
+        "host=postgres connect_timeout=2 user=postgres password=postgresql123 dbname=klustered",
+        NoTls,
+    ) {
+        Ok(c) => c,
+        Err(e) => {
+            return HttpResponse::Ok().body(format!(
+                r#"
+                <html>
+                <head>
+                    <title>v1 | Klustered</title>
+                </head>
+                <body>
+                    <strong>Failed to connect to database</strong>
+                    <p>{}</p>
+                    <iframe src="https://giphy.com/embed/11tTNkNy1SdXGg" width="480" height="267" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/disneypixar-disney-pixar-11tTNkNy1SdXGg">via GIPHY</a></p>
+                </body>
+                </html>
+        "#,
+                e.to_string()
+            ));
+        }
+    };
+
+    let response  = match client.query(sql, &[]) {
+        Ok(result) => {
+            let row = result.first().unwrap();
+
+            let quote: &str = row.get(0);
+            let author: &str = row.get(1);
+            let link: &str = row.get(2);
+
+            HttpResponse::Ok().body(format!(
+                r#"
+                <html>
+                <head>
+                    <title>v1 | Klustered</title>
+                </head>
+                <body>
+                    <center>
+                        <div style="font-size: 20px">
+                            <strong>{}</strong> by <a target="_blank" href="{}">{}</a>
+                        </div>
+                        <video width="720" height="640" controls>
+                            <source src="/assets/video.webm" type="video/webm">
+                        </video>
+                    </center>
+                </body>
+                </html>
+        "#,
+                quote, link, author
+            ))
+        }
+        Err(e) => {
+
+            HttpResponse::Ok().body(format!(
+                r#"
+                <html>
+                <head>
+                    <title>v1 | Klustered</title>
+                </head>
+                <body>
+                    <strong>Failed to query to database</strong>
+                    <p>{}</p>
+                    <iframe src="https://giphy.com/embed/FAYVdONl9am40nLz0o" width="480" height="360" frameBorder="0" class="giphy-embed" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/BTTF-FAYVdONl9am40nLz0o">via GIPHY</a></p>
+                </body>
+                </html>
+        "#,
+                e.to_string(),
+            ))
+        }
+    };
+
+    client.close();
+
+    response
+}
